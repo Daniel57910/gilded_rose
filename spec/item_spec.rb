9dd36884135aca52e0e8cb0a0d70b915362da63e
Daniel Miller <danielmiller5791@gmail.com>
2018-05-24 14:31:42 +0100

    item tested, next refactoring current functionality

---
+++ spec/item_spec.rb
@@ -0,0 +1,13 @@
+require "item"
+require_relative "spec_helper"
+require "pry"
+
+describe "item" do
+  it "returns the name, quality and sell in date of the item" do
+    item = Item.new("cherry", 10, 10)
+    binding.pry
+    expect(item.name).to eq("cherry")
+    expect(item.sell_in).to eq(10)
+    expect(item.quality).to eq(10)
+  end
+end
