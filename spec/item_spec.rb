d38d19edaa8c799ccbe447eeb85ed688d49a7d01
Daniel Miller <danielmiller5791@gmail.com>
2018-05-24 14:31:58 +0100

    item tested, next refactoring current functionality

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -5,7 +5,6 @@ require "pry"
 describe "item" do
   it "returns the name, quality and sell in date of the item" do
     item = Item.new("cherry", 10, 10)
-    binding.pry
     expect(item.name).to eq("cherry")
     expect(item.sell_in).to eq(10)
     expect(item.quality).to eq(10)
