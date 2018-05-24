1bfe7db28cd290654c70782118ab909cc7cb47f3
Daniel Miller <danielmiller5791@gmail.com>
2018-05-24 14:36:26 +0100

    item test refactored and to_s function tested

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -3,10 +3,20 @@ require_relative "spec_helper"
 require "pry"
 
 describe "item" do
+
+  before(:each) do
+     @item = Item.new("cherry", 10, 10)
+  end
   it "returns the name, quality and sell in date of the item" do
-    item = Item.new("cherry", 10, 10)
-    expect(item.name).to eq("cherry")
-    expect(item.sell_in).to eq(10)
-    expect(item.quality).to eq(10)
+    #item = Item.new("cherry", 10, 10)
+    expect(@item.name).to eq("cherry")
+    expect(@item.sell_in).to eq(10)
+    expect(@item.quality).to eq(10)
+  end
+
+  describe "#to_s" do
+    it "returns in string format the name, sell in and quality of the item" do
+      expect(@item.to_s).to eq("cherry, 10, 10")
+    end
   end
 end
