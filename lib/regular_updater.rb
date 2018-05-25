8218e85389b98fce524ad727c3931b151f600643
Daniel Miller <danielmiller5791@gmail.com>
2018-05-25 17:33:18 +0100

    refactored regular item updater. need to continue with irregular items

--- lib/regular_updater.rb
+++ lib/regular_updater.rb
@@ -3,10 +3,18 @@ class Regular_Updater
   attr_reader :items
   def self.update_items(items)
     @@items = items
+    update_regular_items
+  end
+
+  private
+
+  def self.update_regular_items
     @@items.each do | item |
       item.quality -= 1 if item.quality > 0
       item.sell_in -= 1
     end
   end
-  
+
+
+
 end
