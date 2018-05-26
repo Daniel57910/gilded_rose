a9384c549817b583a90a6307651d7c39d30215d6
Daniel Miller <danielmiller5791@gmail.com>
2018-05-26 18:52:44 +0100

    module extracted to pass updater

--- lib/backstage_pass_updater.rb
+++ lib/backstage_pass_updater.rb
@@ -1,10 +1,14 @@
+require ("./lib/modules/item_updater_methods")
+
 class Backstage_Pass_Updater
+
+  extend Item_Updater_Methods
 
   def self.update(pass)
     pass.quality += 1 if pass.sell_in > 10
     pass.quality += 2 if pass.sell_in > 5 and pass.sell_in <= 10
     pass.quality += 3 if pass.sell_in >= 0 and pass.sell_in <=5
-    pass.quality = 0 if pass.sell_in < 0
+    pass.quality = 0 if is_expired?(pass)
   end
 
 end
