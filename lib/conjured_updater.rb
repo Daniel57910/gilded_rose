6275ce65ed38ddcac871c93299e77c49a661abc2
Daniel Miller <danielmiller5791@gmail.com>
2018-05-26 18:58:28 +0100

    conjured item working with module

--- lib/conjured_updater.rb
+++ lib/conjured_updater.rb
@@ -1,6 +1,15 @@
+require ("./lib/modules/item_updater_methods")
+
 class Conjured_Updater
+
+  extend Item_Updater_Methods
+
   def self.update(item)
-    item.quality -= 1 if item.quality > 0
+    if !is_expired?(item) and item_quality_positive?(item) or has_one_quality?(item)
+      item.quality -= 1
+    elsif is_expired?(item) and item_quality_positive?(item)
+      item.quality -= 2
+    end
     item.sell_in -= 2
   end
 end
